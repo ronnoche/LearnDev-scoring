98c24d95e0e7b3765e8a4b1e7a011a0817a22ac9
Cristina Garcia <112044595+ccruitse@users.noreply.github.com>
2022-10-27 20:13:02 +0800

    updated html, css and jscript files

--- script.js
+++ script.js
@@ -1,26 +1,83 @@
-let homeScore = 0;
-let scoreEl = document.getElementById("score");
+let homePts = 0;
+let homeScore = document.getElementById("home-score");
 
+let homeFoulPts = 0;
+let homeFoulScore = document.getElementById("home-foul-score");
 
-function onePoint(){
-    homeScore = homeScore + 1;
-    scoreEl.textContent = homeScore
+let pdPts = 0;
+let pdScore = document.getElementById("period-score");
+
+let guestPts = 0;
+let guestScore = document.getElementById("guest-score");
+
+let guestFoulPts = 0;
+let guestFoulScore = document.getElementById("guest-foul-score");
+
+function homeOnePoint(){
+homePts = homePts + 1;
+homeScore.textContent = homePts
 }
-function twoPoints(){
-    homeScore = homeScore + 2;
-    scoreEl.textContent = homeScore
+function homeTwoPoints(){
+homePts = homePts + 2;
+homeScore.textContent = homePts
 }
-function threePoints(){
-    homeScore = homeScore + 3;
-    scoreEl.textContent = homeScore
+function homeThreePoints(){
+homePts = homePts + 3;
+homeScore.textContent = homePts
+}
+function homeReset(){
+homePts = 0
+homeScore.textContent = 0
+}
+function homeUndo(){
+homePts = homePts + 3;
+homeScore.textContent = homePts
 }
 
-function reset(){
-    homeScore = 0
-    scoreEl.textContent = 0
+function homeFoulOne(){
+homeFoulPts = homeFoulPts + 1;
+homeFoulScore.textContent = homeFoulPts
+}
+function homeFoulReset(){
+homeFoulPts = 0
+homeFoulScore.textContent = 0
 }
 
-function undo(){
-    homeScore = homeScore + 3;
-    scoreEl.textContent = homeScore
+function periodPoint(){
+pdPts = pdPts + 1;
+pdScore.textContent = pdPts
 }
+function periodReset(){
+pdPts = 0
+pdScore.textContent = 0
+}
+
+function guestOnePoint(){
+guestPts = guestPts + 1;
+guestScore.textContent = guestPts
+}
+function guestTwoPoints(){
+guestPts = guestPts + 2;
+guestScore.textContent = guestPts
+}
+function guestThreePoints(){
+guestPts = guestPts + 3;
+guestScore.textContent = guestPts
+}
+function guestReset(){
+guestPts = 0
+guestScore.textContent = 0
+}
+function guestUndo(){
+guestPts = guestPts + 3;
+guestScore.textContent = guestPts
+}
+    
+function guestFoulOne(){
+guestFoulPts = guestFoulPts + 1;
+guestFoulScore.textContent = guestFoulPts
+}
+function guestFoulReset(){
+guestFoulPts = 0
+guestFoulScore.textContent = 0
+}  
