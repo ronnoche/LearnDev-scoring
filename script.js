96ec017e221ce3fef538d76b1207819246827d3e
Cristina Garcia <112044595+ccruitse@users.noreply.github.com>
2022-11-04 19:38:19 +0800

    made changes to repo!!!!!!!

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
+/*HOME*/
 let homePts = 0;
 let homeScore = document.getElementById("home-score");
-
 let homeFoulPts = 0;
 let homeFoulScore = document.getElementById("home-foul-score");
-
-let pdPts = 0;
-let pdScore = document.getElementById("period-score");
-
-let guestPts = 0;
-let guestScore = document.getElementById("guest-score");
-
-let guestFoulPts = 0;
-let guestFoulScore = document.getElementById("guest-foul-score");
 
 function homeOnePoint(){
 homePts = homePts + 1;
@@ -30,27 +21,36 @@ homePts = 0
 homeScore.textContent = 0
 }
 function homeUndo(){
-homePts = homePts + 3;
+homePts = homePts - 1;
 homeScore.textContent = homePts
 }
-
 function homeFoulOne(){
-homeFoulPts = homeFoulPts + 1;
+homeFoulPts = homeFoulPts - 1;
 homeFoulScore.textContent = homeFoulPts
 }
-function homeFoulReset(){
-homeFoulPts = 0
-homeFoulScore.textContent = 0
+function homeFoulUndo(){
+homeFoulPts = homeFoulPts + 1
+homeFoulScore.textContent = homeFoulPts
 }
+
+/*PERIOD*/
+let pdPts = 0;
+let pdScore = document.getElementById("period-score");
 
 function periodPoint(){
 pdPts = pdPts + 1;
 pdScore.textContent = pdPts
 }
 function periodReset(){
-pdPts = 0
-pdScore.textContent = 0
+pdPts = pdPts - 1
+pdScore.textContent = pdPts
 }
+
+/*GUEST*/
+let guestPts = 0;
+let guestScore = document.getElementById("guest-score");
+let guestFoulPts = 0;
+let guestFoulScore = document.getElementById("guest-foul-score");
 
 function guestOnePoint(){
 guestPts = guestPts + 1;
@@ -69,15 +69,14 @@ guestPts = 0
 guestScore.textContent = 0
 }
 function guestUndo(){
-guestPts = guestPts + 3;
+guestPts = guestPts - 1;
 guestScore.textContent = guestPts
-}
-    
+} 
 function guestFoulOne(){
-guestFoulPts = guestFoulPts + 1;
+guestFoulPts = guestFoulPts - 1;
 guestFoulScore.textContent = guestFoulPts
 }
-function guestFoulReset(){
-guestFoulPts = 0
-guestFoulScore.textContent = 0
-}  
+function guestFoulUndo(){
+guestFoulPts = guestFoulPts + 1
+guestFoulScore.textContent = guestFoulPts
+} 
